c8e7d6c594c2bfede21753fb6c20e761406f62b6
Åberg Tomi A <tomiaber@da2-ex-dk107-08.helsinki.fi>
2015-09-08 14:55:19 +0300

    jotain näkymää

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,11 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View:make('helloworld.html');
     }
   }
